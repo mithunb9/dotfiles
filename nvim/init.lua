67076beddc80089eeeb8d7b4c10cf5cc42f39ad9
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2022-11-11 12:25:13 -0600

    update options

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require("mithunb9.core.colorscheme")
+require("mithunb9.core.options")
+require("mithunb9.core.keymaps")
---
+++ nvim/lua/mithunb9/core/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings 
+opt.ignorecase = true
+opt.smartcase = true
+
+

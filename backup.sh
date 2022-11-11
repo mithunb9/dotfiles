bede1928a7e1035ee0c7f1decc73113f3741244f
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2022-11-10 19:36:03 -0600

    update install and backup to be more verbose

--- backup.sh
+++ backup.sh
@@ -1,8 +1,13 @@
 echo Backing up configuration files
 
 # Move into the folder and dump the brew file.
+
+echo "Moving into .dotfiles directory"
 cd ~/.dotfiles
+
+echo "Dumping brewfile"
 rm Brewfile
 brew bundle dump --describe
 
-
+echo "Done!"
+echo "Make sure to commit and push the changes to the repo"
--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
 echo "Installing configuration files"
 
+echo "Starting homebrew"
+
+# Homebrew
+brew bundle --file ~/.dotfiles/Brewfile
+
+echo "Cleaning existing config files"
+# Clean existing configs
+rm -rf ~/.zshrc
+rm -rf ~/.gitconfig
+rm -rf ~/.config/nvim
+
+echo "Syncing config files"
 # Add symlinks
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-# Homebrew
-brew bundle --file ~/.dotfiles/Brewfile
+mkdir .config
+
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+
+echo "Completed"
+
